73119aa48fa4024ed8ef42ee6ed152d56a726618
nyazik <nyaziktenisistka@mail.ru>
2020-12-11 22:50:45 +0300

    last commit

--- TchnFshn/Controllers/TabbarViewController.swift
+++ TchnFshn/Controllers/TabbarViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class TabbarViewController: UITabBarController {
-
+    @IBOutlet weak var back: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         set(name: "opportunity", for: 0)
+        self.navigationController?.isNavigationBarHidden = true
+
     }
     
     func set(name: String, for index: Int) {
